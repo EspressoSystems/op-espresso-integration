5658f9ade4391e5156fa9961c66641e4b008549d
nomaxg <noahgolub2@gmail.com>
2023-09-22 13:23:02 -0400

    lint fixes

--- op-node/p2p/host.go
+++ op-node/p2p/host.go
@@ -7,6 +7,9 @@ import (
 	"sync"
 	"time"
 
+	//nolint:all
+	"github.com/libp2p/go-libp2p/p2p/host/peerstore/pstoreds"
+
 	libp2p "github.com/libp2p/go-libp2p"
 	mplex "github.com/libp2p/go-libp2p-mplex"
 	lconf "github.com/libp2p/go-libp2p/config"
@@ -17,7 +20,6 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/sec/insecure"
 	basichost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	"github.com/libp2p/go-libp2p/p2p/host/peerstore/pstoreds"
 	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
 	"github.com/libp2p/go-libp2p/p2p/security/noise"
 	tls "github.com/libp2p/go-libp2p/p2p/security/tls"
